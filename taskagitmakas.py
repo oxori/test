f32192a0bc512cd3356beab840f87af2d7279fa2
ensar <e>
2020-02-16 17:31:53 +0300

    buglar giderildi

--- taskagitmakas.py
+++ taskagitmakas.py
@@ -1,5 +1,5 @@
 import random
-
+import sys
 wins = 0
 losses = 0
 ties = 0
@@ -9,45 +9,50 @@ while True:
     print('%s Wins, %s Losses, %s Ties' % (wins, losses, ties))
     print('Enter your move: (r)ock (p)aper (s)cissors or (q)uit')
     ch = input()
-    rd = random.choice('psr')
-    # user choices
-    if ch == 'p':
-        print('PAPER versus')
-    elif ch == 's':
-        print('SCISSORS versus')
-    elif ch == 'r':
-        print('ROCK versus')
-    # computer choices
-    if rd == 'p':
-        print('PAPER')
-    elif rd == 's':
-        print('SCISSORS')
-    elif rd == 'r':
-        print('ROCK')
+    if ch in ('p', 's', 'r'):
+        rd = random.choice('psr')
+        # user choices
+        if ch == 'p':
+            print('PAPER versus')
+        elif ch == 's':
+            print('SCISSORS versus')
+        elif ch == 'r':
+            print('ROCK versus')
+        # computer choices
+        if rd == 'p':
+            print('PAPER')
+        elif rd == 's':
+            print('SCISSORS')
+        elif rd == 'r':
+            print('ROCK')
 
-    if ch == rd:
-        print('It is a tie!')
-        ties += 1
+        if ch == rd:
+            print('It is a tie!')
+            ties += 1
+        else:
+            if ch == 'p' and rd == 'r':
+                print('You win!')
+                wins += 1
+            if ch == 'p' and rd == 's':
+                print('You lose!')
+                losses += 1
+
+            if ch == 'r' and rd == 's':
+                print('You win!')
+                wins += 1
+            if ch == 'r' and rd == 'p':
+                print('You lose!')
+                losses += 1
+
+            if ch == 's' and rd == 'p':
+                print('You win!')
+                wins += 1
+            if ch == 's' and rd == 'r':
+                print('You lose!')
+                losses += 1
+        if ch == 'q':
+            break
+    elif ch in ('q'):
+        sys.exit()
     else:
-        if ch == 'p' and rd == 'r':
-            print('You win!')
-            wins += 1
-        if ch == 'p' and rd == 's':
-            print('You lose!')
-            losses += 1
-
-        if ch == 'r' and rd == 's':
-            print('You win!')
-            wins += 1
-        if ch == 'r' and rd == 'p':
-            print('You lose!')
-            losses += 1
-
-        if ch == 's' and rd == 'p':
-            print('You win!')
-            wins += 1
-        if ch == 's' and rd == 'r':
-            print('You lose!')
-            losses += 1
-    if ch == 'q':
-        break
+        print('Type one of r, p, s, or q.')
