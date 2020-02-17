3711b9e63f7947ec850e40df493645094045e25b
ensar <e>
2020-02-18 01:35:22 +0300

    kontrolleri yapıyor, oyunun devam etmesi için while ekle

--- alistirma.py
+++ alistirma.py
@@ -1,2 +1,9 @@
-name='Carol'
-age=3000
+def printPicnic(itemsDict, leftWidth, rightWidth):
+    print('PICNIC ITEMS'.center(leftWidth + rightWidth, '-'))
+    for k, v in itemsDict.items():
+        print(k.ljust(leftWidth, '.') + str(v).rjust(rightWidth))
+
+
+picnicItems = {'sandwiches': 4, 'apples': 12, 'cups': 4, 'cookies': 8000}
+printPicnic(picnicItems, 12, 9)
+printPicnic(picnicItems, 20, 6)
